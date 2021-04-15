7bd308b502d67a64c5ba93eff29727d23b0efedc
rochyscarlata <rosarioscarlata24@gmail.com>
2021-04-15 02:18:03 -0300

    faltan validaciones y q se envie el mail

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Form } from "react-bootstrap";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
 import "./Contact.css";
 function Contact() {
   return (
@@ -11,30 +11,43 @@ function Contact() {
           d="M0,256L80,245.3C160,235,320,213,480,218.7C640,224,800,256,960,256C1120,256,1280,224,1360,208L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
         ></path>
       </svg>
+      <h1 className="tittle-contact">Get in touch</h1>
+        <Row className="contact-form">
+          
+          <Col xs="12" sm={6} className="formulario">
+            <br/>
+          <Form.Group>
+ 
+  <Form.Control type="text" placeholder="Name" className="inp"/>
+  <br/>
+  <Form.Control type="text" placeholder="Email" className="inp"/>
+  <br/>
+  <Form.Control as="textarea" rows={3} placeholder="Message" className="inp"/>
+  <a
+                
+                target="_blank"
+                className="buttonn button--about-me"
+                download="CV-Rosario Scarlata"
+                rel="noreferrer"
+              >
+               SEND
+              </a>
+  </Form.Group>
+          </Col>
+          <Col xs="12" sm={6}>
+         
+        <br></br>
+         <ul className="list"> 
+           <li><Button variant="primary"  className="btn-social" href="https://www.linkedin.com/in/rosario-scarlata-153b831a1/"  target="_blank"><i className="fab fa-linkedin-in"></i></Button> Follow me on linkedin</li>
+           <li><Button variant="danger" className="btn-social" href="mailto:rosarioscarlata24@gmail.com"  target="_blank"><i className="fab fa-google"></i></Button>   Lets chat</li>
+           <li><Button variant="dark" className="btn-social" href="https://github.com/rochyscarlata"  target="_blank"><i className="fab fa-github"></i></Button> Find me on GitHub</li>
 
-      <Form className="contact-form">
-        <h1 className="tittle-contact">Contact me !</h1>
-        <br></br>
-        <Form.Group controlId="exampleForm.ControlInput1" className="mx-sm-3">
-          <Form.Control type="email" placeholder="Name" size="lg" />
-        </Form.Group>
-        <br></br>
-        <Form.Group controlId="exampleForm.ControlInput1" className="mx-sm-3">
-          <Form.Control type="email" placeholder="Email" size="lg" />
-        </Form.Group>
-        <br></br>
-        <Form.Group
-          controlId="exampleForm.ControlTextarea1"
-          className="mx-sm-3 formu"
-        >
-          <Form.Control
-            as="textarea"
-            placeholder="Message"
-            rows={3}
-            size="lg"
-          />
-        </Form.Group>
-      </Form>
+         </ul>
+        
+        
+          </Col>
+        </Row>
+    
     </Container>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,32 +4,7 @@ function Footer() {
   return (
     <div className="footer">
       <footer className="page-footer font-big  pt-4">
-        <div className="container">
-          <ul className="list-unstyled list-inline text-center">
-            <li className="list-inline-item">
-              <a className="btn-floating btn-fb mx-1">
-                <i className="fab fa-facebook-f"> </i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a className="btn-floating btn-tw mx-1">
-                <i className="fab fa-twitter"> </i>
-              </a>
-            </li>
-
-            <li className="list-inline-item">
-              <a className="btn-floating btn-li mx-1">
-                <i className="fab fa-linkedin-in"> </i>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a className="btn-floating btn-dribbble mx-1">
-                <i className="fab fa-dribbble"> </i>
-              </a>
-            </li>
-          </ul>
-        </div>
-
+      
         <div class="footer-copyright text-center py-3">
           © 2021 Copyright:
           <a href="/"> Rosarioscarlatadev.com</a>
